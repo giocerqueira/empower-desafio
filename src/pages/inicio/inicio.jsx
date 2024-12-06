353d72e09a5bcd5d21e31beb5eb15ad108a064aa
Giovanna <giovannacerqueira.09@gmail.com>
2024-12-05 23:21:56 -0300

    correções de responsividade

--- src/pages/inicio/inicio.jsx
+++ src/pages/inicio/inicio.jsx
@@ -8,30 +8,30 @@ import img4 from '../../assets/img4.png'
 export default function Inicio(){
     return(
         <main>
-            <section className={S.post}>
+            <section className={S.background}>
                 <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
             </section>
-            <section className={S.devoDoar}>
+            <section className={S.section2}>
                 <div>
                     <h2>Por que devo doar?</h2>
                 </div>
-                <section className={S.boxCard}>
-                    <article>
+                <section className={S.cards}>
+                    <div>
                         <img src={img1} alt="" />
                         <p> Oferece livros a quem não tem acesso, ajudando a reduzir a exclusão social.</p>
-                    </article>
-                    <article>
+                    </div>
+                    <div>
                         <img src={img2} alt="" />
                         <p>Estimula o hábito da leitura e o aprendizado contínuo.</p>
-                    </article>
-                    <article>
+                    </div>
+                    <div>
                         <img src={img3} alt="" />
                         <p>Fornece conhecimento e inspiração, permitindo que indivíduos transformem suas vidas.</p>
-                    </article>
-                    <article>
+                    </div>
+                    <div>
                         <img src={img4} alt="" />
                         <p>Garante que todos, independentemente de sua condição, tenham oportunidades de aprendizado.</p>
-                    </article>
+                    </div>
                 </section>
             </section>
         </main>
